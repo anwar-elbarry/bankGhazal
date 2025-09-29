426f67d9f6423b76679b2426498933ff0a247d71
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-29 16:33:45 +0100

    feat:create getters and setters / generate random id & numero for the account

--- src/Entity/Compte.java
+++ src/Entity/Compte.java
@@ -1,4 +1,48 @@
 package Entity;
 
+import java.util.UUID;
+
 public sealed class Compte permits CompteEpargne,CompteCourant {
+    protected UUID id;
+    protected String numero;
+    protected double solde;
+    protected UUID idClient;
+    public Compte(double solde, UUID idClient) {
+        setSolde(solde);
+        setIdClient(idClient);
+        setId(UUID.randomUUID());
+        setNumero("CP-"+id.toString().substring(0, 6));
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public double getSolde() {
+        return solde;
+    }
+
+    public void setSolde(double solde) {
+        this.solde = solde;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public UUID getIdClient() {
+        return idClient;
+    }
+
+    public void setIdClient(UUID idClient) {
+        this.idClient = idClient;
+    }
 }
