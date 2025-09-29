822bf179cab5fb49cca1f00ab78a009cb0af4f84
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-29 16:36:54 +0100

    feat:create getters and setters / create constractor

--- src/Entity/CompteCourant.java
+++ src/Entity/CompteCourant.java
@@ -1,4 +1,20 @@
 package Entity;
 
+import java.util.UUID;
+
 public final class CompteCourant extends Compte{
+
+    protected double decouvertAutorise;
+    public CompteCourant(double solde, UUID idClient,double decouvertAutorise) {
+        super(solde, idClient);
+        setDecouvertAutorise(decouvertAutorise);
+    }
+
+    public double getDecouvertAutorise() {
+        return decouvertAutorise;
+    }
+
+    public void setDecouvertAutorise(double decouvertAutorise) {
+        this.decouvertAutorise = decouvertAutorise;
+    }
 }
--- src/Entity/CompteEpargne.java
+++ src/Entity/CompteEpargne.java
@@ -1,4 +1,20 @@
 package Entity;
 
+import java.util.UUID;
+
 public final class CompteEpargne extends Compte {
+    protected double tauxInteret;
+
+    public CompteEpargne(double solde, UUID idClient, double tauxInteret) {
+        super(solde, idClient);
+        setTauxInteret(tauxInteret);
+    }
+
+    public double getTauxInteret() {
+        return tauxInteret;
+    }
+
+    public void setTauxInteret(double tauxInteret) {
+        this.tauxInteret = tauxInteret;
+    }
 }
