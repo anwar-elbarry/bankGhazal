25d6a5d177d96887d99d4e8b8956d622814e3fb7
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-29 16:12:04 +0100

    feat:create connection

--- src/DAO/Util/DatabaseConnection.java
+++ src/DAO/Util/DatabaseConnection.java
@@ -1,4 +1,15 @@
 package DAO.Util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/bankGhazal";
+    private static final String password = "Jppp5734";
+    private static final String username = "postgres";
+
+    public static Connection getConnection()throws SQLException {
+        return DriverManager.getConnection(url,username,password);
+    }
 }
