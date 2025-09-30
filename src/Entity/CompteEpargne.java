abf37948f8af7a9856e66c88bb3241977507ba2a
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-30 14:25:39 +0100

    feat:complet Entities

--- src/Entity/CompteEpargne.java
+++ src/Entity/CompteEpargne.java
@@ -1,12 +1,13 @@
 package Entity;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public final class CompteEpargne extends Compte {
     protected double tauxInteret;
 
-    public CompteEpargne(double solde, UUID idClient, double tauxInteret) {
-        super(solde, idClient);
+    public CompteEpargne(UUID id,String numero,BigDecimal solde, UUID idClient, double tauxInteret) {
+        super(id,numero,solde, idClient);
         setTauxInteret(tauxInteret);
     }
 
--- src/Entity/Transaction.java
+++ src/Entity/Transaction.java
@@ -1,4 +1,8 @@
 package Entity;
 
-public record Transaction() {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record Transaction(UUID id , BigDecimal montant , LocalDateTime date , String lieu , TypeTransaction type, UUID idCompte) {
 }
