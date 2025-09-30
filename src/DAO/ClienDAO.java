f10e166bd9d1eb48b1b501c9db928bd045385d93
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-30 10:51:26 +0100

    feat:complet crud

--- src/DAO/ClienDAO.java
+++ src/DAO/ClienDAO.java
@@ -1,4 +1,53 @@
 package DAO;
 
+import Entity.Client;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class ClienDAO {
+    private Connection connection;
+    public ClienDAO(Connection connection){
+        setConnection(connection);
+    }
+
+    public void addClient(Client client)throws SQLException {
+        String sql = "INSERT INTO client (nom,email) VALUES (?, ?)";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1,client.nom());
+            ps.setString(2,client.email());
+            int result = ps.executeUpdate();
+            if(result == 0){
+                throw new SQLException("Failed to add client");
+            }
+        }
+    }
+    public void modifyClient(Client client)throws SQLException {
+        String sql = "UPDATE client SET nom = ?, email = ? WHERE id = ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1,client.nom());
+            ps.setString(2,client.email());
+            ps.setObject(3,client.id());
+            int result = ps.executeUpdate();
+            if(result == 0){
+                throw new SQLException("Failed to update client");
+            }
+        }
+    }
+
+    public void removeClient(Client client)throws SQLException {
+        String sql = "DELETE FROM client WHERE id = ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setObject(1,client.id());
+            int result = ps.executeUpdate();
+            if(result == 0){
+                throw new SQLException("Failed to remove client");
+            }
+        }
+    }
+
+    public void setConnection(Connection connection){
+        this.connection = connection;
+    }
+
 }
