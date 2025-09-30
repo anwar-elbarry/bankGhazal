c8d0a7e4bc7d274d1732def2df3ba4a1c1f01f48
Anouar El barry <elbarryanwar37@gmail.com>
2025-09-30 15:27:21 +0100

    feat:add validation for ,ame

--- src/Utilitaire/Validator.java
+++ src/Utilitaire/Validator.java
@@ -6,9 +6,15 @@ public class Validator {
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
             "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
     );
+    private static final Pattern NAME_PATTERN = Pattern.compile("^[A-Za-zÀ-ÖØ-öø-ÿ' -]{2,50}$");
 
     public static boolean isValidEmail(String email) {
         if (email == null) return false;
         return EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public static boolean isValidName(String name) {
+        if (name == null) return false;
+        return NAME_PATTERN.matcher(name.trim()).matches();
     }
 }
