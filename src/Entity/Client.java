41968a2a8e028e9e21c50795dd2f82d92db73058
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-01 11:36:03 +0100

    feat:override of toString

--- src/Entity/Client.java
+++ src/Entity/Client.java
@@ -3,4 +3,12 @@ package Entity;
 import java.util.UUID;
 
 public record Client(UUID id,String nom,String email) {
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
