eeecbcb4cd7e264aeecd6bc2da72002109c9c91a
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-02 10:55:53 +0100

    feat:add validation for date

--- src/Utilitaire/Validator.java
+++ src/Utilitaire/Validator.java
@@ -1,8 +1,12 @@
 package Utilitaire;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Validator {
+    private static Scanner sc = new Scanner(System.in);
     private static final Pattern EMAIL_PATTERN = Pattern.compile(
             "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
     );
@@ -16,5 +20,17 @@ public class Validator {
     public static boolean isValidName(String name) {
         if (name == null) return false;
         return NAME_PATTERN.matcher(name.trim()).matches();
+    }
+
+    public static LocalDate askDate(String message) {
+        while (true) {
+            System.out.println(message);
+            String input = sc.nextLine();
+            try {
+                return LocalDate.parse(input);
+            } catch (DateTimeParseException e) {
+                System.out.println("❌ Format invalide. Réessayez (ex: 2025-09-23).");
+            }
+        }
     }
 }
