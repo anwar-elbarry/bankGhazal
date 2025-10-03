e913af8977ed0a2b46dabbf66922384478347a5e
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-03 11:40:29 +0100

    feat:add find transaction by client

--- src/DAO/TransactionDAO.java
+++ src/DAO/TransactionDAO.java
@@ -1,14 +1,14 @@
 package DAO;
 
+import Entity.Client;
 import Entity.Compte;
 import Entity.Transaction;
-import Entity.TypeTransaction;
+import Entity.Enum.TypeTransaction;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 public class TransactionDAO {
     private Connection connection;
@@ -19,12 +19,12 @@ public class TransactionDAO {
     public void addTransaction(Transaction transaction) throws SQLException {
         String sql = "INSERT INTO transaction (id,montant,date,lieu,type,idcompte) VALUES (? ,?, ?, ?, ? ,?)";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
-            ps.setObject(1,transaction.id());
+            ps.setString(1,transaction.id());
             ps.setBigDecimal(2,transaction.montant());
             ps.setTimestamp(3, Timestamp.valueOf(transaction.date()));
             ps.setString(4,transaction.lieu());
             ps.setString(5,transaction.type().toString());
-            ps.setObject(6,transaction.idCompte());
+            ps.setString(6,transaction.idCompte());
             int result = ps.executeUpdate();
             if(result == 0){
                 throw new SQLException("Failed to add transaction");
@@ -35,7 +35,7 @@ public class TransactionDAO {
     public void removeTransaction(Transaction transaction) throws SQLException {
         String sql = "DELETE FROM transaction WHERE id = ?";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
-            ps.setObject(1,transaction.id());
+            ps.setString(1,transaction.id());
             int result = ps.executeUpdate();
             if(result == 0){
                 throw new SQLException("Failed to remove transaction");
@@ -44,13 +44,13 @@ public class TransactionDAO {
     }
 
     public void modifyTransaction(Transaction transaction) throws SQLException {
-        String sql = "UPDATE transaction SET montant = ?, date = ?, lieu = ?, type = ?, id_compte = ? WHERE id = ?";
+        String sql = "UPDATE transaction SET montant = ?, date = ?, lieu = ?, type = ?, idcompte = ? WHERE id = ?";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
             ps.setBigDecimal(1,transaction.montant());
             ps.setString(2,transaction.lieu());
             ps.setObject(3,transaction.type());
-            ps.setObject(4,transaction.idCompte());
-            ps.setObject(5,transaction.id());
+            ps.setString(4,transaction.idCompte());
+            ps.setString(5,transaction.id());
             int result = ps.executeUpdate();
             if(result == 0){
                 throw new SQLException("Failed to update transaction");
@@ -58,25 +58,54 @@ public class TransactionDAO {
         }
     }
 
-    public Optional<Transaction> findTransactionByCompte(Compte compte) throws SQLException {
-        String sql = "SELECT * FROM transaction WHERE id = ?";
+    public List<Transaction> findTransactionByCompte(Compte compte) throws SQLException {
+        String sql = "SELECT * FROM transaction WHERE idcompte = ?";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
-            ps.setObject(1,compte.getId());
+            ps.setString(1,compte.getId());
             try(ResultSet result = ps.executeQuery()){
-                if(result.next()){
+                List<Transaction> transactions = new ArrayList<>();
+                while (result.next()){
                     Transaction transaction = new Transaction(
-                            result.getObject("id",UUID.class),
+                            result.getString("id"),
                             result.getBigDecimal("montant"),
                             result.getTimestamp("date").toLocalDateTime(),
                             result.getString("lieu"),
                             result.getObject("type", TypeTransaction.class),
-                            result.getObject("id_compte",UUID.class)
+                            result.getString("idcompte")
                     );
-                    return Optional.of(transaction);
+                    transactions.add(transaction);
                 }
+                return transactions;
             }
         }
-        return Optional.empty();
+    }
+   public List<Transaction> findTransactionByClient(Client client) throws SQLException {
+        String sql = """
+                SELECT
+                tr.*
+                FROM transaction tr 
+                inner join compte cp on tr.idcompte = cp.id
+                inner join client cl on cp.idclient = cl.id
+                where cl.id = ?
+                """;
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setString(1,client.id());
+            try(ResultSet result = ps.executeQuery()){
+                List<Transaction> transactions = new ArrayList<>();
+                while (result.next()){
+                    Transaction transaction = new Transaction(
+                            result.getString("id"),
+                            result.getBigDecimal("montant"),
+                            result.getTimestamp("date").toLocalDateTime(),
+                            result.getString("lieu"),
+                            result.getObject("type", TypeTransaction.class),
+                            result.getString("idcompte")
+                    );
+                    transactions.add(transaction);
+                }
+                return transactions;
+            }
+        }
     }
 
     public Optional <List<Transaction>> recherchGlobale(String recherche)throws SQLException{
@@ -98,12 +127,12 @@ public class TransactionDAO {
                 List<Transaction> transactions = new ArrayList<>();
                 while(result.next()){
                     Transaction transaction = new Transaction(
-                            result.getObject("id",UUID.class),
+                            result.getString("id"),
                             result.getBigDecimal("montant"),
                             result.getTimestamp("date").toLocalDateTime(),
                             result.getString("lieu"),
                             result.getObject("type", TypeTransaction.class),
-                            result.getObject("id_compte",UUID.class));
+                            result.getString("idcompte"));
                     transactions.add(transaction);
                 }
                 return Optional.of(transactions);
@@ -111,6 +140,29 @@ public class TransactionDAO {
         }
     }
 
+    public List<Transaction> findAll()  throws SQLException{
+        String sql = "SELECT * FROM transaction";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            try(ResultSet result = ps.executeQuery()){
+                List<Transaction> transactions = new ArrayList<>();
+                while (result.next()){
+                    Transaction transaction = new Transaction(
+                            result.getString("id"),
+                            result.getBigDecimal("montant"),
+                            result.getTimestamp("date").toLocalDateTime(),
+                            result.getString("lieu"),
+                            result.getObject("type", TypeTransaction.class),
+                            result.getString("idcompte")
+                    );
+                    transactions.add(transaction);
+                }
+                return transactions;
+            }
+
+        }catch (SQLException e){
+            throw new SQLException("Failed to find all transactions: " + e.getMessage());
+        }
+    }
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
