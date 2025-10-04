9d3c2e2c7fa3ae2e4e97cdb4b416b3034e234cb2
Anouar El barry <elbarryanwar37@gmail.com>
2025-10-04 11:41:14 +0100

    feat:add find by id and find by nome

--- src/DAO/CompteDAO.java
+++ src/DAO/CompteDAO.java
@@ -2,11 +2,14 @@ package DAO;
 
 import Entity.Client;
 import Entity.Compte;
+import Entity.Enum.TypeCompte;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -18,13 +21,16 @@ public class CompteDAO {
     }
 
 
-    public void addCompte(Compte compte)throws SQLException {
-        String sql = "INSERT INTO compte (id,numero,solde,id_client) VALUES (? ,?, ?, ?)";
+    public void addCompte(Compte compte,double tauxInteret,double decouvertAutorise)throws SQLException {
+        String sql = "INSERT INTO compte (id,numero,solde,idclient,typecompte,decouvertautorise,tauxinteret) VALUES (? ,?, ?, ?,?,?,?)";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
-            ps.setObject(1,compte.getId());
+            ps.setString(1,compte.getId());
             ps.setString(2,compte.getNumero());
             ps.setBigDecimal(3,compte.getSolde());
-            ps.setObject(4,compte.getIdClient());
+            ps.setString(4,compte.getIdClient());
+            ps.setString(5,compte.getTypeCompte().toString());
+            ps.setDouble(6,decouvertAutorise);
+            ps.setDouble(7,tauxInteret);
             int result = ps.executeUpdate();
             if(result == 0){
                 throw new SQLException("Failed to add compte");
@@ -36,7 +42,7 @@ public class CompteDAO {
         String sql = "UPDATE compte SET solde = ?  WHERE id = ?";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
             ps.setBigDecimal(1,compte.getSolde());
-            ps.setObject(2,compte.getId());
+            ps.setString(2,compte.getId());
             int result = ps.executeUpdate();
             if(result == 0){
                 throw new SQLException("Failed to update compte");
@@ -48,7 +54,7 @@ public class CompteDAO {
     public void removeCompte(Compte compte)throws SQLException{
         String sql = "DELETE FROM compte WHERE id = ?";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
-            ps.setObject(1,compte.getId());
+            ps.setString(1,compte.getId());
             int result = ps.executeUpdate();
             if(result == 0){
                 throw new SQLException("Failed to remove compte");
@@ -56,23 +62,86 @@ public class CompteDAO {
         }
     }
 
-    public Optional<Compte> findByClient(Client client)throws SQLException{
+    public List<Compte> findAll()throws SQLException{
+        String sql = "SELECT * FROM compte";
+        List<Compte>ClientList = new ArrayList<>();
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+           try(ResultSet result = ps.executeQuery()){
+               while(result.next()){
+                 Compte compte =  new Compte(
+                           result.getString("id"),
+                           result.getString("numero"),
+                           result.getBigDecimal("solde"),
+                           result.getString("idclient"),
+                           result.getString("typecompte").equals("COURANT") ? TypeCompte.COURANT : TypeCompte.EPARGNE
+                   );
+                 ClientList.add(compte);
+               }
+               return ClientList;
+           }
+        }
+    }
+    public Optional<List<Compte>> findByClient(Client client)throws SQLException{
         String sql = "SELECT * FROM compte WHERE id_client = ?";
         try(PreparedStatement ps = connection.prepareStatement(sql)){
             ps.setObject(1,client.id());
            try(ResultSet result = ps.executeQuery()){
-               if (result.next()){
+               List<Compte>clientsList = new ArrayList<>();
+               while (result.next()){
                  Compte compte =  new Compte(
-                           result.getObject("id",UUID.class),
+                           result.getString("id"),
                            result.getString("numero"),
                            result.getBigDecimal("solde"),
-                           result.getObject("id_client",UUID.class)
+                           result.getString("idclient"),
+                           result.getString("typecompte").equals("COURANT") ? TypeCompte.COURANT : TypeCompte.EPARGNE
                    );
-                 return Optional.of(compte);
+                 clientsList.add(compte);
                }
+               return Optional.of(clientsList);
            }
         }
-        return Optional.empty();
+
+    }
+
+    public Optional<Compte> findById(String id)throws SQLException{
+        String sql = "SELECT * FROM compte WHERE id = ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            UUID uuid = UUID.fromString(id);
+            ps.setObject(1,uuid);
+            try(ResultSet result = ps.executeQuery()){
+                if (result.next()){
+                    Compte compte =  new Compte(
+                            result.getString("id"),
+                            result.getString("numero"),
+                            result.getBigDecimal("solde"),
+                            result.getString("idclient"),
+                            result.getString("typecompte").equals("COURANT") ? TypeCompte.COURANT : TypeCompte.EPARGNE
+                    );
+                  return Optional.of(compte);
+                }
+                return Optional.empty();
+            }
+        }
+
+    }public Optional<Compte> findByNumero(String numero)throws SQLException{
+        String sql = "SELECT * FROM compte WHERE numero = ?";
+        try(PreparedStatement ps = connection.prepareStatement(sql)){
+            ps.setObject(1,numero);
+            try(ResultSet result = ps.executeQuery()){
+                if (result.next()){
+                    Compte compte =  new Compte(
+                            result.getString("id"),
+                            result.getString("numero"),
+                            result.getBigDecimal("solde"),
+                            result.getString("idclient"),
+                            result.getString("typecompte").equals("COURANT") ? TypeCompte.COURANT : TypeCompte.EPARGNE
+                    );
+                  return Optional.of(compte);
+                }
+                return Optional.empty();
+            }
+        }
+
     }
 
     public void setConnection(Connection connection){
